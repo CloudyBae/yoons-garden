9d53f5ff3f0f8829669f306188f255bbca9ef250
CloudyBae <yoojinbae93@gmail.com>
2025-09-16 21:50:00 -0500

    change url name

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://cloudybae.github.io',
-	base: "/garden-blog",
+	base: "/yoons-garden",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
